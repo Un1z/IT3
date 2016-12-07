a6bdbe49c3205011121a7d4c149ffc0cf6fef29b
Un1z <robby.ahamad@student.ap.be>
2016-12-07 03:36:15 +0100

    Add files via upload

---
+++ 1617_IT3_AhamadR_Opdracht 8/assets/javascripts/application.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  
+  $(window).scroll(function () {
+      console.log($(window).scrollTop())
+    if ($(window).scrollTop() > 280) {
+      $('#nav_bar').addClass('navbar-fixed');
+    }
+    if ($(window).scrollTop() < 281) {
+      $('#nav_bar').removeClass('navbar-fixed');
+    }
+  });
+});
+
+$(function() {
+  var pull    = $('#pull');
+    menu    = $('nav ul');
+    menuHeight  = menu.height();
+ 
+  $(pull).on('click', function(e) {
+    e.preventDefault();
+    menu.slideToggle();
+  });
+});
+
+$(window).resize(function(){
+  var w = $(window).width();
+  if(w > 320 && menu.is(':hidden')) {
+    menu.removeAttr('style');
+  }
+}); 
